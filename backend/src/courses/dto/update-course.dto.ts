c0f14488b7ce30b819bec1bdd28492702722e797
JongMin999 <ekaloses@naver.com>
2025-12-15 06:21:02 +0900

    코스게시여부 -> 코스상태

--- backend/src/courses/dto/update-course.dto.ts
+++ backend/src/courses/dto/update-course.dto.ts
@@ -16,6 +16,11 @@ export class UpdateCourseDto extends PartialType(CreateCourseDto) {
     @IsString()
     @IsOptional()
     shortDescription?: string;
+
+    @ApiProperty({ description: '코스 상태', required: false })
+    @IsString()
+    @IsOptional()
+    status?: string;
   
     @ApiProperty({ description: '코스 상세페이지 설명', required: false })
     @IsString()
@@ -40,11 +45,6 @@ export class UpdateCourseDto extends PartialType(CreateCourseDto) {
     @IsString()
     @IsOptional()
     level?: string;
-  
-    @ApiProperty({ description: '코스 게시 여부', required: false })
-    @IsBoolean()
-    @IsOptional()
-    isPublished?: boolean;
   
     @ApiProperty({ description: '코스 카테고리 ID 목록', required: false })
     @IsArray()
